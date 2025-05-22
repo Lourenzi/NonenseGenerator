48cf29ac0b2cf80e9527096a9b2d49a7e5fcd295
Lourenzi <83138457+Lourenzi@users.noreply.github.com>
2025-05-22 16:24:32 +0200

    Add files via upload

---
+++ src/main/java/com/ProjectApp/NonSenseGenerator/WebMvcConfigurerAdapter.java
@@ -0,0 +1,12 @@
+package com.ProjectApp.NonSenseGenerator;
+
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+
+public class WebMvcConfigurerAdapter {
+
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addResourceHandlers'");
+    }
+
+}
